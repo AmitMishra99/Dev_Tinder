9132c71f5ef40494537ada63e215af3fb43d2417
Amit Mishra <aamitmishra364@gmail.com>
2026-02-06 20:46:33 +0530

    navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-red-500 p-4">
+      <h1 className="text-2xl">DevTinder</h1>
+    </div>
+  );
+};
+
+export default Navbar;
