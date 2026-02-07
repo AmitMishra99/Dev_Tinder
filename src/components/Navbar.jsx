79d88e1b559412dad44a68bb08cfcb769377b60a
Amit Mishra <aamitmishra364@gmail.com>
2026-02-07 20:29:39 +0530

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const user = useSelector((store) => store.user);
+
   return (
-    <div className="bg-red-500 p-4">
-      <h1 className="text-2xl">DevTinder</h1>
+    <div className="bg-red-500 p-3 flex justify-between items-center">
+      <h1 className="text-3xl ">DevTinder</h1>
+      {user && (
+        <div className="bg-amber-50 w-13 h-13 rounded-full flex items-center justify-center overflow-hidden ">
+          <img
+            className="w-full h-full object-cover object-top"
+            src={user.img}
+            alt="user_img"
+          />
+        </div>
+      )}
     </div>
   );
 };
