aa97a92180e9cae3a1e0599c2addf9e56107957d
Amit Mishra <aamitmishra364@gmail.com>
2026-03-28 15:49:16 +0530

    updated baseURL

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = "http://13.60.56.83/api";
 
 export const defaultPhoto =
   "https://plus.unsplash.com/premium_photo-1677252438411-9a930d7a5168?w=600";
